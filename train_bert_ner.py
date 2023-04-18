239495a6ff8842ecb450ec9dc1c0a0bf0918dcd4
Mikko Lipsanen <lipsanenm@gmail.com>
2023-04-18 15:06:29 +0300

    Update train_bert_ner.py

--- train_bert_ner.py
+++ train_bert_ner.py
@@ -1,23 +1,13 @@
-import json
 import os
 import torch
 import argparse
-import pandas as pd
-import transformers
-import spacy
-from collections import defaultdict
 import numpy as np
-from transformers import BertTokenizerFast, BertForTokenClassification, AutoTokenizer, AutoModelForMaskedLM
+from transformers import BertTokenizerFast, BertForTokenClassification
 from torch.utils.data import Dataset, DataLoader
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 from tqdm import tqdm
-from pathlib import Path
 import matplotlib.pyplot as plt
-from torch.optim import SGD
-import seqeval
 from seqeval.metrics import classification_report
-from  itertools import chain
-from collections import Counter
 
 # Big part of the code is taken and modified from
 # https://towardsdatascience.com/named-entity-recognition-with-bert-in-pytorch-a454405e0b6a
@@ -26,22 +16,22 @@ from collections import Counter
 
 parser = argparse.ArgumentParser('Arguments for training BERT based NER model')
 
-parser.add_argument('--data_path', type=str, default="./data/",
+parser.add_argument('--data_path', type=str, default="/home/ubuntu/NER/ArabicNER/data/tr_val_test/",
                     help='path to data')
-parser.add_argument('--save_model_path', type=str, default="./model/test_model",
+parser.add_argument('--save_model_path', type=str, default="./model/18042023",
                     help='path where the trained model is saved')
 parser.add_argument('--max_len', type=int, default=512,
                     help='Maximum length of data sequence.')
-parser.add_argument('--learning_rate', type=float, default=5e-3,
+parser.add_argument('--learning_rate', type=float, default=0.001,
                     help='Model learning rate.')
-parser.add_argument('--epochs', type=int, default=5,
+parser.add_argument('--epochs', type=int, default=20,
                     help='Number of training epochs.')
-parser.add_argument('--batch_size', type=int, default=2,
+parser.add_argument('--batch_size', type=int, default=8,
                     help='Size of data batch.')
 parser.add_argument('--num_workers', type=int, default=4,
                     help='Number of workers used for the data loaders.')
-parser.add_argument('--val_test_size', type=float, default=0.1,
-                    help='Proportion of data used for validation.')
+parser.add_argument('--patience', type=int, default=2,
+                    help='Number of epochs to train without improvement in selected metric.')
 
 args = parser.parse_args()
 
@@ -52,9 +42,9 @@ device = torch.device("cuda" if use_cuda else "cpu")
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 # Assign numeric ids to named entities
-labels_to_ids = {'O': 0, 'B-PER': 1, 'I-PER': 2, 'B-ORG': 3, 'I-ORG': 4, 'B-LOC': 5, 'I-LOC': 6, 'B-GPE': 7, 'I-GPE':8,
-                 'B-PROD': 9, 'I-PROD': 10, 'B-EVENT': 11, 'I-EVENT': 12, 'B-DATE': 13, 'I-DATE': 14, 'B-JON': 15, 'I-JON': 16, 
-                 'B-FIBC': 17, 'I-FIBC': 18, 'B-NOPR': 19, 'I-NOPR': 20}
+labels_to_ids = {'O': 0, 'B-PERSON': 1, 'I-PERSON': 2, 'B-ORG': 3, 'I-ORG': 4, 'B-LOC': 5, 'I-LOC': 6, 'B-GPE': 7, 'I-GPE': 8,
+                 'B-PRODUCT': 9, 'I-PRODUCT': 10, 'B-EVENT': 11, 'I-EVENT': 12, 'B-DATE': 13, 'I-DATE': 14, 'B-JON': 15, 'I-JON': 16, 
+                 'B-FIBC': 17, 'I-FIBC': 18, 'B-NORP': 19, 'I-NORP': 20}
 ids_to_labels = {v: k for k, v in labels_to_ids.items()}
 
 # Initialize tokenizer and BERT model
@@ -62,28 +52,27 @@ tokenizer = BertTokenizerFast.from_pretrained("TurkuNLP/bert-base-finnish-cased-
 model = BertForTokenClassification.from_pretrained("TurkuNLP/bert-base-finnish-cased-v1", num_labels=len(labels_to_ids.keys()))
 model.to(device)
 
-# Combine annotated excel documents into pandas dataframe
-def excels_to_df(path):
-    path = Path(path)
-    excel_files = list(path.glob('*.xlsx'))
-    all_texts = []
+# Turn conll data into lists of labels and tokens
+def format_conll(conll_path):
+    file_ = open(conll_path, 'r')
+    lines = file_.readlines()
+    all_tokens = []
+    tokens = []
     all_tags = []
-    all_nested_tags = []
-    for i, file in enumerate(excel_files):
-        name = file.stem
-        # Read excel into dataframe
-        df = pd.read_excel(file)
-        df = df.astype(str)
-        text = ' '.join(df['word'].tolist())
-        all_texts.append(text)
-        tags = df['tag'].tolist()
-        all_tags.append(tags)
-        nested_tags = df['nested_tag'].tolist()
-        all_nested_tags.append(nested_tags)
-    # Dataframe with columns for document text, tags and nested tags
-    df = pd.DataFrame(list(zip(all_texts, all_tags, all_nested_tags)), columns =['texts', 'tags', 'nested_tags'])
-    
-    return df
+    tags = []
+    for line in lines:
+        if line != '\n':
+            split_line = line.split(' ')
+            tag = split_line[1].strip('\n')
+            token = split_line[0]
+            tags.append(tag)
+            tokens.append(token)
+        else:
+            all_tokens.append(tokens)
+            all_tags.append(tags)
+            tokens=[]
+            tags=[]
+    return all_tokens, all_tags
 
 
 # Realigns NER labels with BERT tokenization that often splits words into multiple parts
@@ -111,12 +100,9 @@ def align_label(text, labels, label_all_tokens = True):
 
 # Pytorch Dataset for creating batched training data
 class DataSequence(Dataset):
-    def __init__(self, df, max_len, tokenizer):
-        tags = df['tags'].values.tolist()
-        texts = df['texts'].values.tolist()
+    def __init__(self, tags, texts, max_len, tokenizer):
         self.max_len = max_len
-        self.texts = [tokenizer(str(i),
-                               padding='max_length', max_length = self.max_len, truncation=True, return_tensors="pt") for i in texts]
+        self.texts = [tokenizer(text, padding='max_length', max_length = self.max_len, truncation=True, return_tensors="pt", is_split_into_words=True) for text in texts]
         self.labels = [align_label(i,j) for i,j in zip(self.texts, tags)]
 
     def __len__(self):
@@ -135,32 +121,22 @@ class DataSequence(Dataset):
         return batch_data, batch_labels
 
 
-def tags(df):
-    # Get count of the number of tags per category
-    s = pd.Series(list(chain.from_iterable(df['tags'])))
-    print('NER tags in annotation data: \n', s.value_counts())
-    # Replace all tags not included in the annotation list with 'O'
-    df['tags'] =  df['tags'].apply(lambda x: [tag if tag in list(labels_to_ids.keys()) else 'O' for tag in x])
-    s2 = pd.Series(list(chain.from_iterable(df['tags'])))
-    print('\n NER tags in cleaned annotation data: ', s2.value_counts())
-    n_unique = len(s2.unique())
-    return n_unique
+# Format conll data into Pytorch datasets and dataloaders
+def get_data(data_path, tokenizer):
+    tr_tokens, tr_tags = format_conll(data_path + 'train.txt')
+    val_tokens, val_tags = format_conll(data_path + 'val.txt')
+    test_tokens, test_tags = format_conll(data_path + 'val.txt')
 
-
-def split_data(df, tokenizer):
-    # Split data into train, validation and test datasets
-    df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42),
-                            [int((1-2*args.val_test_size) * len(df)), int((1-args.val_test_size) * len(df))])
-
-    print("FULL Dataset: {}".format(df.shape))
-    print("TRAIN Dataset: {}".format(df_train.shape))
-    print("VALIDATION Dataset: {}".format(df_val.shape))
-    print("TEST Dataset: {}".format(df_test.shape))
+    print("TRAIN Dataset: {}".format(sum([len(t) for t in tr_tokens])))
+    print("VALIDATION Dataset: {}".format(sum([len(t) for t in val_tokens])))
+    print("TEST Dataset: {}".format(sum([len(t) for t in test_tokens])))
+    print('\n')
 
     # Create train, validation and test datasets
-    train_dataset = DataSequence(df_train, args.max_len, tokenizer)
-    val_dataset = DataSequence(df_val, args.max_len, tokenizer)
-    test_dataset = DataSequence(df_test, args.max_len, tokenizer)
+    train_dataset = DataSequence(tr_tags, tr_tokens, args.max_len, tokenizer)
+    val_dataset = DataSequence(val_tags, val_tokens, args.max_len, tokenizer)
+    test_dataset = DataSequence(test_tags, test_tokens, args.max_len, tokenizer)
+
     # Create train, validation and test dataloaders
     train_dataloader = DataLoader(train_dataset, num_workers=args.num_workers, batch_size=args.batch_size, shuffle=True)
     val_dataloader = DataLoader(val_dataset, num_workers=args.num_workers, batch_size=args.batch_size)
@@ -179,24 +155,46 @@ def test_init_loss(model, dataset, idx, num_classes):
 
     print('Initial loss: ', outputs[0].item())
     print('-ln(1/number of classes) = ', -np.log(1/num_classes))
+    print('\n')
 
 def test_alignment(tokenizer, dataset, idx):
     batch_data, batch_labels = dataset[idx]
     for token, label in zip(tokenizer.convert_ids_to_tokens(batch_data["input_ids"][0]), batch_labels):
-        print('{0:10}  {1}'.format(token, label))
+        if label.item() != -100:
+            print('{0:10}  {1}'.format(token, ids_to_labels[label.item()]))
+    print('\n')
+
+def format_labels(labels, logits):
+    # compute evaluation accuracy
+    flattened_targets = labels.view(-1) # shape (batch_size * seq_len,)
+    active_logits = logits.view(-1, model.num_labels) # shape (batch_size * seq_len, num_labels)
+    flattened_predictions = torch.argmax(active_logits, axis=1) # shape (batch_size * seq_len,)
+            
+    # only compute accuracy at active labels
+    active_accuracy = labels.view(-1) != -100 # shape (batch_size, seq_len)
+        
+    labels = torch.masked_select(flattened_targets, active_accuracy)
+    predictions = torch.masked_select(flattened_predictions, active_accuracy)
+
+    # Change numeric tags to text format
+    tags = [ids_to_labels[id.item()] for id in labels]
+    preds = [ids_to_labels[id.item()] for id in predictions]
+
+    return tags, preds
 
 # Function for training the model
-def train_loop(model, optimizer, train_dataloader, val_dataloader, bs, epochs):
-    best_acc = 0
-    best_loss = 1000
+def train_loop(model, optimizer, scheduler, train_dataloader, val_dataloader, epochs):
+    best_val_loss = 1000
+    epoch_n = 0
+    timestep = 0
 
     tr_acc_history = []
     val_acc_history = []
     val_loss_history = []
     tr_loss_history = []
 
-    n_train = len(train_dataloader.dataset)
-    n_val = len(val_dataloader.dataset)
+    n_train = len(train_dataloader)
+    n_val = len(val_dataloader)
 
     for epoch_num in range(epochs):
         total_acc_train = 0
@@ -205,7 +203,7 @@ def train_loop(model, optimizer, train_dataloader, val_dataloader, bs, epochs):
         model.train()
         
         # Training loop
-        for train_data, train_label in tqdm(train_dataloader):
+        for b, (train_data, train_label) in enumerate(tqdm(train_dataloader)):
             train_label = train_label.to(device)
             mask = train_data['attention_mask'].squeeze(1).to(device)
             input_id = train_data['input_ids'].squeeze(1).to(device)
@@ -225,18 +223,28 @@ def train_loop(model, optimizer, train_dataloader, val_dataloader, bs, epochs):
             loss.backward()
             optimizer.step()
 
+            if timestep % 10 == 0:
+                    print("Epoch %d | Batch %d/%d | Timestep %d | LR %.10f | Loss %f"%(epoch_num,b,n_train,timestep,optimizer.param_groups[0]['lr'],loss.item()))
+
+        # Avoids exploding gradient by doing gradient clipping
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
         model.eval()
 
         total_acc_val = 0
         total_loss_val = 0
 
         # Validation loop
-        for val_data, val_label in tqdm(val_dataloader):
+        val_preds, val_labels = [], []
+        for batch, (val_data, val_label) in enumerate(tqdm(val_dataloader)):
             val_label = val_label.to(device)
             mask = val_data['attention_mask'].squeeze(1).to(device)
             input_id = val_data['input_ids'].squeeze(1).to(device)
 
             loss, logits = model(input_ids=input_id, attention_mask=mask, labels=val_label, return_dict=False)
+            labels, predictions = format_labels(val_label, logits)
+            val_labels.append(labels)
+            val_preds.append(predictions)
 
             for i in range(logits.shape[0]):
                 logits_clean = logits[i][val_label[i] != -100]
@@ -257,13 +265,30 @@ def train_loop(model, optimizer, train_dataloader, val_dataloader, bs, epochs):
         tr_acc_history.append(tr_accuracy)
         tr_loss_history.append(tr_loss)
 
-
         print(
             f'Epochs: {epoch_num + 1} \
             | Loss: {tr_loss: .3f} \
             | Accuracy: {tr_accuracy: .3f} \
             | Val_Loss: {val_loss: .3f} \
-            | Accuracy: {val_accuracy: .3f}')
+            | Val_accuracy: {val_accuracy: .3f}')
+
+        print('\n')
+        print(classification_report(val_labels, val_preds, zero_division=1))
+
+        # Saves model if validation accuracy improves
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            epoch_n = epoch_num
+            print('Saving model to ', args.save_model_path)
+            # Save trained model
+            model.save_pretrained(args.save_model_path, from_pt=True)
+            epoch_n = epoch_num
+
+        elif (epoch_num - epoch_n) >= args.patience:
+            print('Training is aborted as validation loss has not improved')
+            break
+
+        scheduler.step()
 
 
     hist_dict = {'tr_acc': tr_acc_history, 
@@ -271,8 +296,7 @@ def train_loop(model, optimizer, train_dataloader, val_dataloader, bs, epochs):
                 'val_loss': val_loss_history,
                 'tr_loss': tr_loss_history}
 
-
-    return model, hist_dict
+    return hist_dict
 
 
 # Function for evaluating trained model with test data
@@ -293,26 +317,14 @@ def evaluate(model, test_dataloader):
 
         nb_eval_steps += 1
         nb_eval_examples += labels.size(0)
-              
-        # compute evaluation accuracy
-        flattened_targets = labels.view(-1) # shape (batch_size * seq_len,)
-        active_logits = logits.view(-1, model.num_labels) # shape (batch_size * seq_len, num_labels)
-        flattened_predictions = torch.argmax(active_logits, axis=1) # shape (batch_size * seq_len,)
-            
-        # only compute accuracy at active labels
-        active_accuracy = labels.view(-1) != -100 # shape (batch_size, seq_len)
-        
-        labels = torch.masked_select(flattened_targets, active_accuracy)
-        predictions = torch.masked_select(flattened_predictions, active_accuracy)
-            
-        test_labels.extend(labels)
-        test_preds.extend(predictions)
-        
-        tmp_eval_accuracy = accuracy_score(labels.cpu().numpy(), predictions.cpu().numpy())
-        eval_accuracy += tmp_eval_accuracy
 
-    labels = [ids_to_labels[id.item()] for id in test_labels]
-    preds = [ids_to_labels[id.item()] for id in test_preds]
+        tags, predictions = format_labels(labels, logits)
+         
+        test_labels.append(tags)
+        test_preds.append(predictions)
+        
+        tmp_eval_accuracy = accuracy_score(np.array(tags), np.array(predictions))
+        eval_accuracy += tmp_eval_accuracy
 
     eval_loss = eval_loss / nb_eval_steps
     eval_accuracy = eval_accuracy / nb_eval_steps
@@ -320,24 +332,8 @@ def evaluate(model, test_dataloader):
     print(f"Validation Accuracy: {eval_accuracy}")
     
     # Reports model performance for each tag category
-    print(classification_report([labels], [preds], zero_division=1))
-
-
-def evaluate_one_text(model, sentence):
-    """Function for using the trained model to predict NER tags for given input."""
-    text = tokenizer(sentence, padding='max_length', max_length = args.max_len, truncation=True, return_tensors="pt")
-
-    mask = text['attention_mask'].to(device)
-    input_id = text['input_ids'].to(device)
-    label_ids = torch.Tensor(align_label(sentence)).unsqueeze(0).to(device)
-
-    logits = model(input_ids=input_id, attention_mask=mask, labels=None, return_dict=False)
-    logits_clean = logits[0][label_ids != -100]
-
-    predictions = logits_clean.argmax(dim=1).tolist()
-    prediction_label = [ids_to_labels[i] for i in predictions]
-    print(sentence)
-    print(prediction_label)
+    print(classification_report(test_labels, test_preds, zero_division=1))
+    print('\n')
 
 
 def plot_metrics(hist_dict):
@@ -363,27 +359,21 @@ def plot_metrics(hist_dict):
 
 
 def main():
-    # Combine annotation excels into a dataframe
-    df = excels_to_df(args.data_path)
-    df = df.sample(n = 100)
-    n_unique = tags(df)
 
-    print('Number of documents in data: ', len(df))
+    train_dataset, val_dataset, test_dataset, train_dataloader, val_dataloader, test_dataloader = get_data(args.data_path, tokenizer)
 
-    train_dataset, val_dataset, test_dataset, train_dataloader, val_dataloader, test_dataloader = split_data(df, tokenizer)
-
-    test_init_loss(model, train_dataset, 5, n_unique)
-
-    test_alignment(tokenizer, train_dataset, 5)
+    n_unique_tags = len(list(labels_to_ids.keys()))
+    test_init_loss(model, train_dataset, 55, n_unique_tags)
+    test_alignment(tokenizer, train_dataset, 55)
 
     #optimizer = SGD(model.parameters(), lr=args.learning_rate)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=1, last_epoch=-1, verbose=True)
 
     # Train the model
-    trained_model, hist_dict = train_loop(model, optimizer, train_dataloader, val_dataloader, args.batch_size, args.epochs)
+    hist_dict = train_loop(model, optimizer, scheduler, train_dataloader, val_dataloader, args.epochs)
 
-    # Save trained model
-    trained_model.save_pretrained(args.save_model_path, from_pt=True)
+    trained_model = BertForTokenClassification.from_pretrained(args.save_model_path, num_labels=n_unique_tags)
 
     # Evaluate model with test data
     evaluate(trained_model, test_dataloader)

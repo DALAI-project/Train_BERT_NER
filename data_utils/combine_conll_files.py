e37754c9e7a3b617a64fb9ba3a69e8b9d4732309
Mikko Lipsanen <lipsanenm@gmail.com>
2023-08-18 14:24:35 +0300

    Update combine_conll_files.py

--- data_utils/combine_conll_files.py
+++ data_utils/combine_conll_files.py
@@ -1,16 +1,21 @@
 import argparse
+import os
 
 parser = argparse.ArgumentParser('Arguments for the code')
 
 parser.add_argument('-l','--list', action='append', help='paths to input files', required=True)
-parser.add_argument('--save_path', type=str, default="./data/conll-data/combined_data.txt",
+parser.add_argument('--save_path', type=str, default="./data/conll-data/",
                     help='path for saving combined conll file')
+parser.add_argument('--name', type=str, default="combined_data.txt",
+                    help='name for the combined conll file')  
 
 args = parser.parse_args()
 
 # Joins multiple annotation files in conll-format into one text file
-def combine_conll_files(output_path, input_paths):    
-    with open(output_path, 'w') as outfile:
+def combine_conll_files(output_path, name, input_paths):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)   
+    with open(output_path + name, 'w') as outfile:
         for i, fname in enumerate(input_paths):
             with open(fname) as infile:
                 for line in infile:
@@ -21,6 +26,6 @@ def combine_conll_files(output_path, input_paths):
 
 
 def main():
-    combine_conll_files(args.save_path, args.list)
+    combine_conll_files(args.save_path, args.name, args.list)
 
 main()
